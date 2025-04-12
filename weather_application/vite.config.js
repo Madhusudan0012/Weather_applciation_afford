fdeaf38821c8480bd6f9e31205f6e59c8295a09a
Madhusudan <madhusudanchauhan62@gmail.com>
2025-04-12 15:31:41 +0530

    deploy changes

--- weather_application/vite.config.js
+++ weather_application/vite.config.js
@@ -5,5 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react({
     jsxRuntime: 'automatic', // ✅ Enables new JSX transform
+    base : "/weather_forecast"
   })],
 });
